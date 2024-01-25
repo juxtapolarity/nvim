01996f311e1d9f8041f0eb59cbe96c8b8f63a8e6
Julian Felding (JULS) <juls@demant.com>
2024-01-25 12:47:07 +0100

    Text remains highlighted when indenting

--- lua/juls/remap.lua
+++ lua/juls/remap.lua
@@ -18,3 +18,7 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
+
+-- reselect line after indentation
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
