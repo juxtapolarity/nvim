a4a60de9e6e0836862f7f2df3f7f09b87ea274fc
Julian Felding (JULS) <juls@demant.com>
2026-01-20 11:48:27 +0100

    Migrate obsidian to obsidian-nvim/obsidian.nvim
    Description:
    The older obsidian plugin created by epwalch is no longer actively
    maintained, so I need to migrate to an actively maintained fork.

--- lua/jux/lazy.lua
+++ lua/jux/lazy.lua
@@ -219,20 +219,10 @@ lazy.setup({
     },
 
     {
-      "epwalsh/obsidian.nvim",
-      version = "*",  -- recommended, use latest release instead of latest commit
-      lazy = true,
-      ft = "markdown",
-      -- event = {
-      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-      --   "BufReadPre " .. vim.fn.expand "~" .. "obsidian/juxnotes/**.md",
-      --   "BufNewFile " .. vim.fn.expand "~" .. "obsidian/juxnotes/**.md",
-      -- },
-      dependencies = {
-        -- Required.
-        "nvim-lua/plenary.nvim",
-      },
+      "obsidian-nvim/obsidian.nvim",
+      version = "*",
+      lazy = false,
+      dependencies = { "nvim-lua/plenary.nvim" },
     },
 
     -- {
